de490668a7d96f316f87082c55f2684f4c3865b5
orhanors <orsorhan1@gmail.com>
2024-04-30 12:28:39 +0300

    ready to test

--- binance_limit.js
+++ binance_limit.js
@@ -6,7 +6,7 @@ const axios = require("axios")
 const BASE_URL = 'https://api.binance.com';
 const PAIR = 'TNSRUSDT';
 // const AMOUNT_USDT = "45";
-const LISTING_TIME = 1714468980000 - 310; // CHECK MEE
+const LISTING_TIME = 1714478400000 - 310; // CHECK MEE
 
 
 const queryString = (obj) => {
